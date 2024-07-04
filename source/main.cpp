d9e5f41f9aaf06fe7ed0505a91e1b3be4d3336ce
Damien Zhao <zdm65477730@126.com>
2024-07-04 21:37:46 +0800

    fix build error
    Signed-off-by: Damien Zhao <zdm65477730@126.com>

--- source/main.cpp
+++ source/main.cpp
@@ -944,7 +944,7 @@ public:
 	virtual tsl::elm::Element* createUI() override {
 		// A OverlayFrame is the base element every overlay consists of. This will draw the default Title and Subtitle.
 		// If you need more information in the header or want to change it's look, use a HeaderOverlayFrame.
-		auto frame = new tsl::elm::OverlayFrame("PluginName"_tr, APP_VERSION);
+		auto frame = new tsl::elm::OverlayFrame("PluginName"_tr, VERSION);
 
 		// A list that can contain sub elements and handles scrolling
 		auto list = new tsl::elm::List();
