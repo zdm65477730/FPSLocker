ee45c6338ce873d90b96b249533e363f81986523
Damien Zhao <zdm65477730@126.com>
2025-01-12 13:25:36 +0800

    add multi-lang support
    Signed-off-by: Damien Zhao <zdm65477730@126.com>

--- source/Modes/Advanced.hpp
+++ source/Modes/Advanced.hpp
@@ -3,12 +3,12 @@ public:
     SetBuffers() {}
 
     virtual tsl::elm::Element* createUI() override {
-		auto frame = new tsl::elm::OverlayFrame("NVN Set Buffering", " ");
+		auto frame = new tsl::elm::OverlayFrame("NVNSetBufferingSetBuffersOverlayFrame"_tr, " ");
 
 		auto list = new tsl::elm::List();
-		list->addItem(new tsl::elm::CategoryHeader("It will be applied on next game boot.", false));
-		list->addItem(new tsl::elm::NoteHeader("Remember to save settings after change.", true, {0xF, 0x3, 0x3, 0xF}));
-		auto *clickableListItem = new tsl::elm::ListItem2("Double");
+		list->addItem(new tsl::elm::CategoryHeader("AppliedNextGameBootSetBuffersListItemCategoryHeader"_tr, false));
+		list->addItem(new tsl::elm::NoteHeader("RememberSaveSettingsSetBuffersListItemNoteHeader"_tr, true, {0xF, 0x3, 0x3, 0xF}));
+		auto *clickableListItem = new tsl::elm::ListItem2("DoubleSetBuffersListItem"_tr);
 		clickableListItem->setClickListener([](u64 keys) { 
 			if ((keys & HidNpadButton_A) && PluginRunning) {
 				SetBuffers_save = 2;
@@ -20,7 +20,7 @@ public:
 		list->addItem(clickableListItem);
 
 		if ((Shared -> SetActiveBuffers) == 2 && (Shared -> Buffers) == 3 && !SetBuffers_save) {
-			auto *clickableListItem2 = new tsl::elm::ListItem2("Triple (force)");
+			auto *clickableListItem2 = new tsl::elm::ListItem2("TripleForceSetBuffersListItem"_tr);
 			clickableListItem2->setClickListener([](u64 keys) { 
 				if ((keys & HidNpadButton_A) && PluginRunning) {
 					SetBuffers_save = 3;
@@ -32,7 +32,7 @@ public:
 			list->addItem(clickableListItem2);
 		}
 		else {
-			auto *clickableListItem2 = new tsl::elm::ListItem2("Triple");
+			auto *clickableListItem2 = new tsl::elm::ListItem2("TripleSetBuffersListItem"_tr);
 			clickableListItem2->setClickListener([](u64 keys) { 
 				if ((keys & HidNpadButton_A) && PluginRunning) {
 					if ((Shared -> Buffers) == 4) SetBuffers_save = 3;
@@ -47,7 +47,7 @@ public:
 		
 		if ((Shared -> Buffers) == 4) {
 			if ((Shared -> SetActiveBuffers) < 4 && (Shared -> SetActiveBuffers) > 0 && (Shared -> Buffers) == 4) {
-				auto *clickableListItem3 = new tsl::elm::ListItem2("Quadruple (force)");
+				auto *clickableListItem3 = new tsl::elm::ListItem2("QuadrupleForceSetBuffersListItem"_tr);
 				clickableListItem3->setClickListener([](u64 keys) { 
 					if ((keys & HidNpadButton_A) && PluginRunning) {
 						SetBuffers_save = 4;
@@ -59,7 +59,7 @@ public:
 				list->addItem(clickableListItem3);	
 			}
 			else {
-				auto *clickableListItem3 = new tsl::elm::ListItem2("Quadruple");
+				auto *clickableListItem3 = new tsl::elm::ListItem2("QuadrupleSetBuffersListItem"_tr);
 				clickableListItem3->setClickListener([](u64 keys) { 
 					if ((keys & HidNpadButton_A) && PluginRunning) {
 						SetBuffers_save = 0;
@@ -83,14 +83,14 @@ public:
     SyncMode() {}
 
     virtual tsl::elm::Element* createUI() override {
-        auto frame = new tsl::elm::OverlayFrame("NVN Window Sync Wait", "Mode");
+        auto frame = new tsl::elm::OverlayFrame("NVNWindowSyncWaitSyncModeOverlayFrame"_tr, "SyncModeSyncModeOverlayFrameSubtitle"_tr);
 
 		auto list = new tsl::elm::List();
 
-		auto *clickableListItem = new tsl::elm::ListItem2("Enabled");
+		auto *clickableListItem = new tsl::elm::ListItem2("EnabledSyncModeSyncModeListItem"_tr);
 		clickableListItem->setClickListener([](u64 keys) { 
 			if ((keys & HidNpadButton_A) && PluginRunning) {
-				ZeroSyncMode = "On";
+				ZeroSyncMode = "OnSyncModeAdvancedGuiListItemText"_tr.c_str();
 				(Shared -> ZeroSync) = 0;
 				tsl::goBack();
 				tsl::goBack();
@@ -100,10 +100,10 @@ public:
 		});
 		list->addItem(clickableListItem);
 
-		auto *clickableListItem2 = new tsl::elm::ListItem2("Semi-Enabled");
+		auto *clickableListItem2 = new tsl::elm::ListItem2("SemiEnabledSyncModeSyncModeListItem"_tr);
 		clickableListItem2->setClickListener([](u64 keys) { 
 			if ((keys & HidNpadButton_A) && PluginRunning) {
-				ZeroSyncMode = "Semi";
+				ZeroSyncMode = "SemiSyncModeAdvancedGuiListItemText"_tr.c_str();
 				(Shared -> ZeroSync) = 2;
 				tsl::goBack();
 				tsl::goBack();
@@ -113,10 +113,10 @@ public:
 		});
 		list->addItem(clickableListItem2);
 
-		auto *clickableListItem3 = new tsl::elm::ListItem2("Disabled");
+		auto *clickableListItem3 = new tsl::elm::ListItem2("DisabledSyncModeSyncModeListItem"_tr);
 		clickableListItem3->setClickListener([](u64 keys) { 
 			if ((keys & HidNpadButton_A) && PluginRunning) {
-				ZeroSyncMode = "Off";
+				ZeroSyncMode = "OffSyncModeAdvancedGuiListItemText"_tr.c_str();
 				(Shared -> ZeroSync) = 1;
 				tsl::goBack();
 				tsl::goBack();
@@ -139,48 +139,48 @@ public:
 		configValid = LOCK::readConfig(&configPath[0]);
 		if (R_FAILED(configValid)) {
 			if (configValid == 0x202) {
-				sprintf(&lockInvalid[0], "Game config file not found\nTID: %016lX\nBID: %016lX", TID, BID);
+				sprintf(&lockInvalid[0], "ConfigFileNotFoundAdvancedGuiCustomDrawerText"_tr.c_str(), TID, BID);
 			}
-			else sprintf(&lockInvalid[0], "Game config error: 0x%X", configValid);
+			else sprintf(&lockInvalid[0], "ConfigErrorAdvancedGuiCustomDrawerText"_tr.c_str(), configValid);
 		}
 		else {
 			patchValid = checkFile(&patchPath[0]);
 			if (R_FAILED(patchValid)) {
 				if (!FileDownloaded) {
 					if (R_SUCCEEDED(configValid)) {
-						sprintf(&patchChar[0], "Patch file doesn't exist.\nUse \"Convert config to patch file\"\nto make it!");
+						sprintf(&patchChar[0], "PatchFileNotExistMoreAdvancedGuiCustomDrawerText"_tr.c_str());
 					}
-					else sprintf(&patchChar[0], "Patch file doesn't exist.");
+					else sprintf(&patchChar[0], "PatchFileNotExistAdvancedGuiCustomDrawerText"_tr.c_str());
 				}
 				else {
-					sprintf(&patchChar[0], "New config downloaded successfully.\nUse \"Convert config to patch file\"\nto make it applicable!");
+					sprintf(&patchChar[0], "NewConfigDownloadSuccessAdvancedGuiCustomDrawerText"_tr.c_str());
 				}
 			}
-			else sprintf(&patchChar[0], "Patch file exists.");
+			else sprintf(&patchChar[0], "PatchFileExistAdvancedGuiCustomDrawerText"_tr.c_str());
 		}
 		switch((Shared -> ZeroSync)) {
 			case 0:
-				ZeroSyncMode = "On";
+				ZeroSyncMode = "OnSyncModeAdvancedGuiListItemText"_tr.c_str();
 				break;
 			case 1:
-				ZeroSyncMode = "Off";
+				ZeroSyncMode = "OffSyncModeAdvancedGuiListItemText"_tr.c_str();
 				break;
 			case 2:
-				ZeroSyncMode = "Semi";
+				ZeroSyncMode = "SemiSyncModeAdvancedGuiListItemText"_tr.c_str();
 		}
 	}
 
 	size_t base_height = 134;
 
     virtual tsl::elm::Element* createUI() override {
-        auto frame = new tsl::elm::OverlayFrame("FPSLocker", "Advanced settings");
+        auto frame = new tsl::elm::OverlayFrame("PluginName"_tr, "AdvancedSettingsAdvancedGuiOverlayFrameSubTitle"_tr);
 
 		auto list = new tsl::elm::List();
 
 		if ((Shared -> API)) {
 			switch((Shared -> API)) {
 				case 1: {
-					list->addItem(new tsl::elm::CategoryHeader("GPU API Interface: NVN", false));
+					list->addItem(new tsl::elm::CategoryHeader("NVNAdvancedGuiCategoryHeader"_tr, false));
 					
 					list->addItem(new tsl::elm::CustomDrawer([this](tsl::gfx::Renderer *renderer, s32 x, s32 y, s32 w, s32 h) {
 						
@@ -189,7 +189,7 @@ public:
 					}), 40);
 
 					if ((Shared -> Buffers) == 2 || (Shared -> SetBuffers) == 2 || (Shared -> ActiveBuffers) == 2) {
-						auto *clickableListItem3 = new tsl::elm::MiniListItem("Window Sync Wait", ZeroSyncMode);
+						auto *clickableListItem3 = new tsl::elm::MiniListItem("SyncWaitAdvancedGuiListItem"_tr, ZeroSyncMode);
 						clickableListItem3->setClickListener([](u64 keys) { 
 							if ((keys & HidNpadButton_A) && PluginRunning) {
 								tsl::changeTo<SyncMode>();
@@ -200,7 +200,7 @@ public:
 						list->addItem(clickableListItem3);
 					}
 					if ((Shared -> Buffers) > 2) {
-						auto *clickableListItem3 = new tsl::elm::MiniListItem("Set Buffering");
+						auto *clickableListItem3 = new tsl::elm::MiniListItem("SetBufferingAdvancedGuiListItem"_tr);
 						clickableListItem3->setClickListener([](u64 keys) { 
 							if ((keys & HidNpadButton_A) && PluginRunning) {
 								tsl::changeTo<SetBuffers>();
@@ -213,14 +213,14 @@ public:
 					break;
 				}
 				case 2:
-					list->addItem(new tsl::elm::CategoryHeader("GPU API Interface: EGL", false));
+					list->addItem(new tsl::elm::CategoryHeader("EGLAdvancedGuiCategoryHeader"_tr, false));
 					break;
 				case 3:
-					list->addItem(new tsl::elm::CategoryHeader("GPU API Interface: Vulkan", false));
+					list->addItem(new tsl::elm::CategoryHeader("VulkanAdvancedGuiCategoryHeader"_tr, false));
 			}
 		}
 
-		list->addItem(new tsl::elm::CategoryHeader("FPSLocker Patches", false));
+		list->addItem(new tsl::elm::CategoryHeader("PatchesAdvancedGuiCategoryHeader"_tr, false));
 
 		if (R_FAILED(configValid)) {
 			base_height = 154;
@@ -230,7 +230,7 @@ public:
 			
 			if (R_SUCCEEDED(configValid)) {
 				
-				renderer->drawString("Found valid config file!", false, x, y+20, 20, renderer->a(0xFFFF));
+				renderer->drawString("ConfigFileValidAdvancedGuiCustomDrawerText"_tr.c_str(), false, x, y+20, 20, renderer->a(0xFFFF));
 				renderer->drawString(&patchAppliedChar[0], false, x, y+40, 20, renderer->a(0xFFFF));
 				if (R_FAILED(patchValid)) {
 					renderer->drawString(&patchChar[0], false, x, y+64, 20, renderer->a(0xF99F));
@@ -246,28 +246,28 @@ public:
 		}), base_height);
 
 		if (R_SUCCEEDED(configValid)) {
-			list->addItem(new tsl::elm::NoteHeader("Remember to reboot the game after conversion!", true, {0xF, 0x3, 0x3, 0xF}));
-			auto *clickableListItem = new tsl::elm::MiniListItem("Convert config to patch file");
+			list->addItem(new tsl::elm::NoteHeader("PatchWillBeAppliedNextGameBootAdvancedGuiNoteHeader"_tr, true, {0xF, 0x3, 0x3, 0xF}));
+			auto *clickableListItem = new tsl::elm::MiniListItem("ConvertConfigToPatchFileAdvancedGuiListItem"_tr);
 			clickableListItem->setClickListener([](u64 keys) { 
 				if ((keys & HidNpadButton_A) && PluginRunning) {
 					patchValid = LOCK::createPatch(&patchPath[0]);
 					if (R_SUCCEEDED(patchValid)) {
-						sprintf(&patchChar[0], "Patch file created successfully.\nRestart the game and change\nFPS Target to apply the patch!");
+						sprintf(&patchChar[0], "PatchFileCreatedSuccessAdvancedGuiListItemText"_tr.c_str());
 					}
-					else sprintf(&patchChar[0], "Error while creating patch: 0x%x", patchValid);
+					else sprintf(&patchChar[0], "PatchFileCreateFailedAdvancedGuiListItemText"_tr.c_str(), patchValid);
 					return true;
 				}
 				return false;
 			});
 			list->addItem(clickableListItem);
 
-			auto *clickableListItem2 = new tsl::elm::MiniListItem("Delete patch file");
+			auto *clickableListItem2 = new tsl::elm::MiniListItem("DeletePatchFileAdvancedGuiListItem"_tr);
 			clickableListItem2->setClickListener([](u64 keys) { 
 				if ((keys & HidNpadButton_A) && PluginRunning) {
 					if (R_SUCCEEDED(patchValid)) {
 						remove(&patchPath[0]);
 						patchValid = 0x202;
-						sprintf(&patchChar[0], "Patch file deleted successfully.");
+						sprintf(&patchChar[0], "DeletePatchSuccessfulFileAdvancedGuiListItemText"_tr.c_str());
 					}
 					return true;
 				}
@@ -276,13 +276,13 @@ public:
 			list->addItem(clickableListItem2);
 		}
 		if (R_FAILED(configValid)) {
-			list->addItem(new tsl::elm::NoteHeader("This can take up to 30 seconds.", true, {0xF, 0x3, 0x3, 0xF}));
+			list->addItem(new tsl::elm::NoteHeader("Take30sAdvancedGuiListItemText"_tr, true, {0xF, 0x3, 0x3, 0xF}));
 		}
-		auto *clickableListItem4 = new tsl::elm::MiniListItem("Check/download config file");
+		auto *clickableListItem4 = new tsl::elm::MiniListItem("CheckOrDownloadConfigFileAdvancedGuiListItemText"_tr);
 		clickableListItem4->setClickListener([this](u64 keys) { 
 			if ((keys & HidNpadButton_A) && PluginRunning && exitPossible) {
 				exitPossible = false;
-				sprintf(&patchChar[0], "Checking Warehouse for config...\nExit not possible until finished!");
+				sprintf(&patchChar[0], "CheckWarehouseAdvancedGuiListItemText"_tr.c_str());
 				threadCreate(&t1, downloadPatch, NULL, NULL, 0x20000, 0x3F, 3);
 				threadStart(&t1);
 				return true;
@@ -302,15 +302,15 @@ public:
 		if (PluginRunning) {
 			if (i > 9) {
 				if ((Shared -> patchApplied) == 1) {
-					sprintf(patchAppliedChar, "Patch was loaded to game.");
+					sprintf(patchAppliedChar, "PluginLoadedUpdateAdvancedGuiCustomDrawerText"_tr.c_str());
 				}
 				else if ((Shared -> patchApplied) == 2) {
-					sprintf(patchAppliedChar, "Master Write was loaded to game.");
+					sprintf(patchAppliedChar, "MasterWriteLoadedUpdateAdvancedGuiCustomDrawerText"_tr.c_str());
 				}
-				else sprintf(patchAppliedChar, "Plugin didn't apply patch to game.");
+				else sprintf(patchAppliedChar, "PluginNotApplyUpdateAdvancedGuiCustomDrawerText"_tr.c_str());
 				if ((Shared -> API) == 1) {
 					if (((Shared -> Buffers) >= 2 && (Shared -> Buffers) <= 4)) {
-						sprintf(&nvnBuffers[0], "Set/Active/Available buffers: %d/%d/%d", (Shared -> SetActiveBuffers), (Shared -> ActiveBuffers), (Shared -> Buffers));
+						sprintf(&nvnBuffers[0], "SetOrActiveOrAvailableBuffersUpdateAdvancedGuiCustomDrawerText"_tr.c_str(), (Shared -> SetActiveBuffers), (Shared -> ActiveBuffers), (Shared -> Buffers));
 					}
 				}
 				i = 0;
@@ -337,43 +337,43 @@ public:
 				error_code = UINT32_MAX;
 			}
 			if (rc == 0x316) {
-				sprintf(&patchChar[0], "Connection timeout!");
+				sprintf(&patchChar[0], "ConnectionTimeoutAdvancedGuiListItemText"_tr.c_str());
 			}
 			else if (rc == 0x212 || rc == 0x312) {
-				sprintf(&patchChar[0], "Config is not available! RC: 0x%x", rc);
+				sprintf(&patchChar[0], "ConfigNotAvailableAdvancedGuiListItemText"_tr.c_str(), rc);
 			}
 			else if (rc == 0x404) {
-				sprintf(&patchChar[0], "Config is not available!\nChecking Warehouse for more info...\nExit not possible until finished!");
+				sprintf(&patchChar[0], "ConfigNotAvailable404AdvancedGuiListItemText"_tr.c_str());
 			}
 			else if (rc == 0x405) {
-				sprintf(&patchChar[0], "Config is not available!\nChecking Warehouse for more info...\nTimeout! It took too long to check.");
+				sprintf(&patchChar[0], "ConfigNotAvailableTimeoutAdvancedGuiListItemText"_tr.c_str());
 			}
 			else if (rc == 0x406) {
-				sprintf(&patchChar[0], "Config is not available!\nChecking Warehouse for more info...\nConnection error!");
+				sprintf(&patchChar[0], "ConfigNotAvailableConnectionErrorAdvancedGuiListItemText"_tr.c_str());
 			}
 			else if (rc == 0x104) {
-				sprintf(&patchChar[0], "No new config available.");
+				sprintf(&patchChar[0], "NoNewConfigAdvancedGuiListItemText"_tr.c_str());
 			}
 			else if (rc == 0x412) {
-				sprintf(&patchChar[0], "Internet connection not available!");
+				sprintf(&patchChar[0], "InternetConnectionNotAvailableAdvancedGuiListItemText"_tr.c_str());
 			}
 			else if (rc == 0x1001) {
-				sprintf(&patchChar[0], "Patch is not needed for this game!");
+				sprintf(&patchChar[0], "PatchNotNeededAdvancedGuiListItemText"_tr.c_str());
 			}
 			else if (rc == 0x1002) {
-				sprintf(&patchChar[0], "This game is not listed in Warehouse!");
+				sprintf(&patchChar[0], "NotListedInWarehouseAdvancedGuiListItemText"_tr.c_str());
 			}
 			else if (rc == 0x1003) {
-				sprintf(&patchChar[0], "This game is listed in Warehouse,\nbut with different version. Other\nversion doesn't need a patch, your\nversion maybe doesn't need it too!");
+				sprintf(&patchChar[0], "DiffVersionListedInWarehouseNoNeedPatchAdvancedGuiListItemText"_tr.c_str());
 			}
 			else if (rc == 0x1004) {
-				sprintf(&patchChar[0], "This game is listed in Warehouse,\nbut with different version.\nOther version recommends patch,\nbut config is not available even for it!");
+				sprintf(&patchChar[0], "DiffVersionListedInWarehouseNoPatchAdvancedGuiListItemText"_tr.c_str());
 			}
 			else if (rc == 0x1005) {
-				sprintf(&patchChar[0], "This game is listed in Warehouse,\nbut with different version.\nOther version has config available!");
+				sprintf(&patchChar[0], "DiffVersionListedInWarehouseOtherVersionAvailableAdvancedGuiListItemText"_tr.c_str());
 			}
 			else if (rc == 0x1006) {
-				sprintf(&patchChar[0], "This game is listed in Warehouse\nwith patch recommended for this\nversion, but config is not available!");
+				sprintf(&patchChar[0], "ListedInWarehousePatchNotAvailableAdvancedGuiListItemText"_tr.c_str());
 			}
 			else if (R_SUCCEEDED(rc)) {
 				FILE* fp = fopen(patchPath, "rb");
@@ -386,7 +386,7 @@ public:
 				return true;
 			}
 			else if (rc != UINT32_MAX) {
-				sprintf(&patchChar[0], "Connection error! RC: 0x%x", rc);
+				sprintf(&patchChar[0], "ConnectionErrorAdvancedGuiListItemText"_tr.c_str(), rc);
 			}
 		}
         return false;   // Return true here to signal the inputs have been consumed
